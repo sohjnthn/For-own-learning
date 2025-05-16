f503f3355435dac2b549197376255b07a8c596ad
sohjnthn <sohjnthn@gmail.com>
2025-05-17 07:10:39 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // To avoid making changes for both Cucumber Java and actual Test.java (the file which uses Cucumber's Feature - Scenario - Given/Then/When file)
+    }
+}
